1396607e7342772bcf2fedab81e46bb3076632e0
Windigo <hodosikalman@gmail.com>
2026-06-13 10:10:04 +0100

    Project first commit

---
+++ mixedmessage.js
@@ -0,0 +1,25 @@
+//random message generator
+
+function generateFirstWord() {
+ const firstWords = ["World", "God", "Buddha", "Jesus", "Allah"];
+ const randomIndex = Math.floor(Math.random() * firstWords.length);
+ return firstWords[randomIndex];
+ }
+
+
+function generateSecondWord() {
+ const SecondWords = ["heals", "ignores", "loves", "plays", "guides"];
+ const randomIndex = Math.floor(Math.random() * SecondWords.length);
+ return SecondWords[randomIndex];
+ }
+
+
+function generateThirdWord() {
+ const thirdWords = ["stupids", "innovation", "cool stuff", "bamboo", "technology"];
+ const randomIndex = Math.floor(Math.random() * thirdWords.length);
+ return thirdWords[randomIndex];
+ }
+
+ for (let i = 0; i < 3; i++) {
+  console.log(generateFirstWord() + " " + generateSecondWord() + " " + generateThirdWord());
+ }
